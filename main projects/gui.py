2f577fc34dc758d9ba65077b0e83dceac11bfd29
Serafim <seraoriginal21@mail.com>
2025-06-05 03:34:16 +0300

    changed theme added clock, handle edit and complete errors

--- main projects/gui.py
+++ main projects/gui.py
@@ -1,29 +1,31 @@
-from gc import enable
-
 import functions
-import FreeSimpleGUI
-# if i want to dont write FreeSimpleGui all over again i can just write import FreeSimpleGui as sg and replace the FreeSimpleGui below
-label = FreeSimpleGUI.Text("Type in a To-Do")
-input_box = FreeSimpleGUI.InputText(tooltip="Enter a todo", key="todo")
-add_button = FreeSimpleGUI.Button("Add")
-list_box = FreeSimpleGUI.Listbox(values=functions.get_todos(), key='todos',
-                                 enable_events=True, size=[45, 10])
-edit_button = FreeSimpleGUI.Button("Edit")
-complete_button = FreeSimpleGUI.Button("Complete")
-exit_button = FreeSimpleGUI.Button("Exit")
+import FreeSimpleGUI as sg
+import time
 
-window = FreeSimpleGUI.Window('My To-Do App',
-                              layout=[[label],
+
+# if i want to dont write FreeSimpleGui all over again i can just write import FreeSimpleGui as sg and replace the FreeSimpleGui below
+sg.theme("black")
+
+clock = sg.Text('', key='clock')
+label = sg.Text("Type in a To-Do")
+input_box = sg.InputText(tooltip="Enter a todo", key="todo")
+add_button = sg.Button("Add", size=10)
+list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+                                 enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
+
+window = sg.Window('My To-Do App',
+                              layout=[[clock],[label],
                                       [input_box, add_button],
                                       [list_box, edit_button, complete_button],
                                       [exit_button]],
                               font=('Helvetica', 20))
 
 while True:
-    event, values = window.read()
-    print(1, event)
-    print(2, values)
-    print(3, values['todos'])
+    event, values = window.read(timeout=10)
+    window['clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -33,28 +35,35 @@ while True:
             window['todos'].update(values=todos)
 
         case "Edit":
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo']
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo']
 
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+            except IndexError:
+                sg.popup("Please select an item first", font=("Helvetica", 20))
+
         case "Complete":
-            todo_to_complete = values['todos'][0]
-            todos = functions.get_todos()
-            todos.remove(todo_to_complete)
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
-            window['todo'].update(value='')
+            try:
+                todo_to_complete = values['todos'][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value='')
+            except IndexError:
+                sg.popup("Please select an item first", font=("Helvetica", 20))
 
         case "Exit":
             break
 
         case 'todos':
             window['todo'].update(value=values['todos'][0])
-        case FreeSimpleGUI.WIN_CLOSED:
+        case sg.WIN_CLOSED:
             break
 
 
